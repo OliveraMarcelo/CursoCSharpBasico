824e0ce73e46f4f1f8f3d5c93954fe2fd936a3fc
Marcelo Olivera <olivera.m.et13@gmail.com>
2022-01-29 18:44:32 -0300

    Subiendo clases

---
+++ EstructuradeControl2-Jump/Program.cs
@@ -0,0 +1,85 @@
+﻿
+
+namespace Estructuras2
+{
+    class Program
+    {
+        static void mostrarLista(List<int> v)
+        {
+            foreach (int x in v)
+            {
+                //Con salto, continue
+                /*
+                 *  if(x % 2 != 0)
+                {
+                    continue
+                }
+                Console.Write(x + "");
+                 */
+                //Sin salto 
+                if (x % 2 == 0)
+                {
+                    Console.Write(x + " ");
+                }
+            }
+            Console.WriteLine("");
+
+        }
+        static void buscarNum(int x, List<int> v)
+        {
+            /*
+             Lo que haremos es : 
+            1utilizaremos un indice que va pasar por la lista 
+            2comparar cada numero que saquemos de la lista con el numero que nos pasaron como parametros
+             */
+            bool encontrado = false;
+            for (int i = 0; i < v.Count && !encontrado; i++)
+            {
+                /*
+              * Sin break
+                if (v[i] == x)
+                {
+                    Console.WriteLine("El numero : " + x + "esta en la lista");
+                }
+                Console.Write(i + " ");
+                
+                */
+                //Con break para que no nos siga mostrando los numeros de la lista,
+                //y Cuando encuentre el numero salte el ciclo y siga con las instrucciones que le siguen
+                if (v[i] == x)
+                {
+                    Console.WriteLine("El numero : " + x + " esta en la lista");
+                    encontrado = true;
+                }
+                Console.Write(i + " ");
+            }
+            Console.WriteLine("");
+        }
+        static void Main(string[] args)
+        {
+            List<int> lista = new List<int>();
+            for (int i = 1; i <= 40; i++)
+            {
+                lista.Add(i);
+            }
+            mostrarLista(lista);
+            buscarNum(5, lista);
+            int ind = 0;
+            //En este caso el incremento no se ejecuta nunca y estamos en un loop infinito
+            // Por lo tanto la condicion se mantiene todo el tiempo verdadera
+            while (ind < 40)
+            {
+                if(lista[ind]!=6)
+                {
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine("El numero 6 esta en la lista");
+                }
+                ind++;
+            }
+
+        }
+    }
+}
---
+++ EstructurasDeControl3/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+namespace Estructuras3
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            int[] vector = { 1, 2, 3 , 4 ,5 };
+            for(int i = 0; i <= vector.Length; i++)
+            {
+                try
+                {
+                Console.WriteLine(vector[i]);
+                }
+                catch (Exception ex) //Conviene ser mas especifico entonces cambiamos a que puede ser nu error de INDEXOUTOFRANGEEXCEPTION
+                {
+                    Console.WriteLine("Nos pasamos de rango, valor del indice " + i);  
+                }
+                finally //Finally sirve para ejecutando texto
+                {}
+
+            }
+            Console.Write("[");
+            for(int i = 0 ; i < vector.Length ; i++)
+            {
+                Console.Write (vector[i]);
+                if(i < vector.Length - 1)
+                {
+                    Console.Write(", ");
+                }
+            }
+            Console.Write("]");
+
+        }
+    }
+}
